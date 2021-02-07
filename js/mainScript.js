11ed97b0991207fcd8f0c78fc19d6eb3e8567b26
Ibrahim ADLANI <ibrahim@adlani.com>
2021-02-07 02:21:59 +0100

    addRow() : Fonction qui ajoute des lignes dans le playfield.

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,5 +1,5 @@
-const lignes = 0;
-const colonnes = 0;
+const lignes = 4;
+const colonnes = 2;
 const emojis = ["🤓","😟","😒","😹","😄","🎃","✌🏽","😳","😭","🤡","👺","🤣","👹","🦫"]
 
 var cnt = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
@@ -7,7 +7,13 @@ var clicks = 0;
 var selected = [];
 var paires = 0;
 
+// Fonction qui ajoute des lignes dans le "playfield" //
+function addRow(){
+    var parent = document.getElementById("playfield");
+    var row = document.createElement("div");
+    row.classList = ["row"];
+    parent.appendChild(row);
 
+    return row
+}
 
-
-$(".card").flip();
